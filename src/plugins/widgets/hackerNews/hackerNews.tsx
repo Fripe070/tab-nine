655415469a9aabb00df6c32cd7f37ee4e5c33fc4
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2025-02-18 19:37:40 +0100

    More hackernews links

--- src/plugins/widgets/hackerNews/hackerNews.tsx
+++ src/plugins/widgets/hackerNews/hackerNews.tsx
@@ -7,57 +7,77 @@ import "./HackerNews.scss";
 
 
 function NthMinute(interval: number, time: Date): number {
-    return Math.floor(time.getTime() / (1000 * 60 * interval));
+  return Math.floor(time.getTime() / (1000 * 60 * interval));
 }
 
 const EXPIRE_IN = MINUTES * 15;
 
 const HackerNewsWidget: FC<Props> = ({
-    cache,
-    data = defaultData,
-    setCache,
-    loader,
+  cache,
+  data = defaultData,
+  setCache,
+  loader,
 }) => {
-    useCachedEffect(
-        () => {
-            getStories(loader, data.count).then(stories => setCache({
-                stories: stories,
-                cachedAt: Date.now(),
-            }));
-        },
-        cache ? cache.cachedAt + EXPIRE_IN : 0,
-        [data.count],
-    );
-    if (!cache) return null;
+  useCachedEffect(
+    () => {
+      getStories(loader, data.count).then(stories => setCache({
+        stories: stories,
+        cachedAt: Date.now(),
+      }));
+    },
+    cache ? cache.cachedAt + EXPIRE_IN : 0,
+    [data.count],
+  );
+  if (!cache) return null;
 
-    return (
-        <div className="HackerNews">
-            <div className="stories">
-                {cache.stories.map(story => (
-                    <Story key={story.id} story={story} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="HackerNews">
+      <div className="stories">
+        {cache.stories.map(story => (
+          <Story key={story.id} story={story} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HackerNewsWidget;
 
 
 function Story({ story }: { story: Story }) {
-    return (
-        <a
-            className="story"
-            href={story.url}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <div className="title">
-                  {story.title}
-            </div>
-            <p className="by">
-                Posted by {story.by}
-            </p>
-        </a>
-    );
+  return (
+    <div className="story">
+      <a
+        className="title"
+        href={story.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {story.title}
+      </a>
+      <p className="info">
+        Posted by {SimpleURL(
+          `https://news.ycombinator.com/user?id=${story.id}`,
+          story.by
+        )}
+        {" "}
+        |
+        {" "}
+        {SimpleURL(
+          `https://news.ycombinator.com/item?id=${story.id}`,
+          `${story.descendants} comments`
+        )}
+      </p>
+    </div>
+  );
+}
+
+function SimpleURL(url: string, content: string) {
+  return <a
+    href={url}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {content}
+  </a>
 }
